fe9904ddf3660929fc733c43e6bde2bd6f6cf11a
halimgambit <70286759+halimgambit@users.noreply.github.com>
2026-06-29 21:01:40 +0200

    Update action.Reminder.js

--- Reminder/action.Reminder.js
+++ Reminder/action.Reminder.js
@@ -2,12 +2,31 @@ import {default as _helpers} from '../../ia/node_modules/ava-ia/helpers/index.js
 
 export default function (state) {
 	return new Promise((resolve) => {
+	
+     try {
+
+ const stopWords = ["éteins", "coupe", "stop", "stoppe", "arrête", "annule"];
+const stopReminder = stopWords.some(word => state.rawSentence.toLowerCase().includes(word));
+
 		setTimeout(() => { 
+      if (stopReminder) {
+          state.action = {
+            module: 'Reminder',
+            command: 'stopReminder',
+          };
+           } else {
+          if (state.debug) info('Action Reminder');
 			state.action = {
 				module: 'Reminder',
-				command: state.rule
+				command: state.rule,
 			};
+    };
 			resolve(state);
 		}, Config.waitAction.time);
+
+     } catch (error) {
+      reject(new Error(`Une erreur s'est produite lors du traitement de la commande Reminder: ${error.message}`));
+    }
+
 	});
 }
